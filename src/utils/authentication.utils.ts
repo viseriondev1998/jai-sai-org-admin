9692c5af1c18ec2ff9e5e5b7caa7719d91391404
mistryaksh <111272027+mistryaksh@users.noreply.github.com>
2024-03-29 19:07:16 +0530

    Initial commit

---
+++ src/utils/authentication.utils.ts
@@ -0,0 +1,20 @@
+import { fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+export const SetAuthToken = (token: string) => {
+     return localStorage.setItem("ADMIN", token);
+};
+
+export const GetAuthToken = () => {
+     return localStorage.getItem("ADMIN");
+};
+
+export const RemoveAuthToken = () => {
+     return localStorage.removeItem("ADMIN");
+};
+
+export const AppBaseQuery = fetchBaseQuery({
+     baseUrl: "https://om-shanti-walfare-trust-backend-o06r.onrender.com/api/1.0",
+     prepareHeaders(headers, api) {
+          headers.set("Authorization", GetAuthToken()?.toString() as string);
+     },
+});
